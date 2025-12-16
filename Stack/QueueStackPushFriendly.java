00963e092d4cb26314ae6c778dec7fd3c8b865a4
bawantha200 <bawantha200@gmail.com>
2025-12-16 20:50:35 +0530

    Add files via upload

---
+++ Stack/QueueStackPushFriendly.java
@@ -0,0 +1,115 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package QueueStackPushFriendly;
+
+/**
+ *
+ * @author Bawantha
+ */
+
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.NoSuchElementException;
+
+public class QueueStackPushFriendly {
+
+    private Queue<Integer> mainQueue = new LinkedList<>();
+    private Queue<Integer> tempQueue = new LinkedList<>();
+    
+    public QueueStackPushFriendly(){
+        mainQueue = new LinkedList<>();
+        tempQueue = new LinkedList<>();
+    }
+    
+    public void push(int x){
+        mainQueue.add(x); // enqueue
+    }
+    
+    public boolean empty(){
+        return mainQueue.isEmpty(); // isEmpty
+    }
+    
+    public int pop(){
+        if(empty()){
+            throw new NoSuchElementException("Stack is empty!");
+        }
+        
+        while(mainQueue.size()>1){
+            tempQueue.add(mainQueue.remove()); //moves all element except the last one from maineQueue to tempQueue
+        }
+        
+        int popped = mainQueue.remove(); // dequeue
+        
+        Queue<Integer> temp = mainQueue;
+        mainQueue = tempQueue; // mainQueue now point to tempQueue
+        tempQueue = temp; // empty tempQueue for next operation
+        
+        return popped;
+    }
+    
+    public int peek(){
+         if(empty()){
+            throw new NoSuchElementException("Stack is empty!");
+        }
+        
+        while(mainQueue.size()>1){
+            tempQueue.add(mainQueue.remove()); //moves all element except the last one from maineQueue to tempQueue
+        }
+        
+        int top = mainQueue.peek(); // peek
+        tempQueue.add(mainQueue.remove()); // put element back
+        
+        Queue<Integer> temp = mainQueue;
+        mainQueue = tempQueue; // mainQueue now point to tempQueue
+        tempQueue = temp; // empty tempQueue for next operation
+        
+        return top;
+    }
+    
+    
+    public static void main(String[] args) {
+        // TODO code application logic here
+        QueueStackPushFriendly s = new QueueStackPushFriendly();
+        
+        System.out.println("Testing Queue Stack (Push Friendly)\n");
+        System.out.println("[  ]");
+        System.out.println("isEmpty : " + s.empty());
+        
+        s.push(3);
+        System.out.println("Push : 3");
+        s.push(5);
+        System.out.println("Push : 5");
+        s.push(7);
+        System.out.println("Push : 7");
+        System.out.println("[3,5,7]");
+        System.out.println("= = = = = = = =");
+        
+        try {
+            System.out.println("Pop : " + s.pop()); // remove 7
+        } catch (NoSuchElementException e) {
+            System.out.println("Cannot pop - stack is empty. Moving to next operation...");
+        }
+        System.out.println("[3,5]\n");
+        
+        s.push(9);
+        System.out.println("Push : 9");
+        System.out.println("[3,5,9]\n");
+
+        try {
+            System.out.println("Pop : " + s.pop()); // remove 9
+            System.out.println("[3,5]\n");
+            
+            System.out.println("Pop : " + s.pop()); // remove 5
+            System.out.println("[3]\n");
+            
+            System.out.println("Peek : " + s.peek()); // show 3
+           
+        } catch (NoSuchElementException e) {
+            System.out.println("Cannot pop - stack is empty. Moving to next operation...");
+        }   
+        System.out.println("isEmpty : " + s.empty());
+    }
+    
+}
---
+++ Stack/StackQueueEnqueueFriendly.java
@@ -0,0 +1,104 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package StackQueueEnqueueFriendly;
+
+/**
+ *
+ * @author Bawantha
+ */
+
+import java.util.NoSuchElementException;
+import java.util.Stack;
+
+public class StackQueueEnqueueFriendly {
+    
+    private Stack<Integer> enStack = new Stack<>();
+    private Stack<Integer> deStack = new Stack<>();
+    
+    public StackQueueEnqueueFriendly(){
+        enStack = new Stack<>();
+        deStack = new Stack<>();
+    }
+    
+    public void enqueue(int x){
+        enStack.push(x);
+    }
+    
+    public int dequeue(){
+        if(enStack.isEmpty()){
+            throw new NoSuchElementException("Queue is empty!");
+        }
+        
+        while(enStack.size()>1){
+            deStack.push(enStack.pop());
+        }
+        
+        int dequeued = enStack.pop();
+        
+        while (!deStack.isEmpty()){
+            enStack.push(deStack.pop());
+        }
+        
+        return dequeued;
+    }
+    
+    public int peek(){
+        if(enStack.empty()){
+            throw new NoSuchElementException("Queue is empty!");
+        }
+        if(deStack.isEmpty()){
+            while (!enStack.isEmpty()){
+                deStack.push(enStack.pop());
+            }
+        }
+        return deStack.peek();
+    }
+    
+    public boolean empty(){
+        return enStack.isEmpty() && deStack.isEmpty();
+    }
+
+    
+    public static void main(String[] args) {
+        // TODO code application logic here
+        StackQueueEnqueueFriendly q = new StackQueueEnqueueFriendly();
+        
+        System.out.println("Testing Stack Queue Enequeue Friendly\n");
+        System.out.println("isEmpty : " + q.empty());
+        System.out.println("[  ]");
+        q.enqueue(3);
+        System.out.println("Enqueue : 3");
+        q.enqueue(5);
+        System.out.println("Enqueue : 5");
+        System.out.println("[ 3 , 5 ]");
+        System.out.println("= = = = = =");
+        
+        try {
+            System.out.println("Dequeue : " + q.dequeue());
+            System.out.println("[ 5 ]");
+            System.out.println("= = = = = =");
+        } catch (NoSuchElementException e) {
+            System.out.println("Cannot pop - stack is empty. Moving to next operation...");
+        }
+
+        q.enqueue(7);
+        System.out.println("Enqueue : 7");
+        System.out.println("[ 5 , 7 ]");
+        System.out.println("= = = = = =");
+
+        try {
+            System.out.println("Dequeue : " + q.dequeue());
+            System.out.println("[ 7 ]");
+            System.out.println("= = = = = =");
+        
+            System.out.println("Peek : " + q.peek());
+        } catch (NoSuchElementException e) {
+            System.out.println("Cannot pop - stack is empty. Moving to next operation...");
+        }
+        
+        System.out.println("isEmpty : " + q.empty());
+    }
+    
+}
